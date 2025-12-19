edcf3efa60ff560b3dd04f5ed27ef71ce5546967
Prateek Gupta <prateekjpg@gmail.com>
2025-12-19 17:13:42 +0530

    add people/ route, serializer and view code

--- chat/urls.py
+++ chat/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+
+from .views import UserList
+
 urlpatterns = [
+    path('people/', UserList.as_view(), name='people'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import mixins, generics, permissions
+
+from authentication.authentication import CookieJWTAuthentication
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class UserList(mixins.ListModelMixin, generics.GenericAPIView):
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        return self.list(request)
