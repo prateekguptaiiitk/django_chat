36f5be5a50149b16e24ebe555c92675da70e537e
Prateek Gupta <prateekjpg@gmail.com>
2025-12-26 14:32:48 +0530

    change channel backend to external redis layer

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -102,9 +102,18 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels.layers.InMemoryChannelLayer',
+#     },
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
